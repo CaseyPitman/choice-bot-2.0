1bb58314b4bd39a5d81b7a2547fd0bc2075aebf9
Casey The Great <casey@caseypitman.com>
2020-09-02 12:55:45 -0500

    header styles complete

--- src/App.js
+++ src/App.js
@@ -17,29 +17,32 @@ class App extends Component {
       <div className='App'>
 
         <header className='header'>
-          <h1 className ='headline'>Choicebot 2.0</h1>
-          <h2 className = 'subHeadline'>Let me decide for you indecisive human. </h2>
-          <img className = 'robotImage' src={require('./choice-bot.jpg')} alt = 'The Choicebot'/>
+          <div className = 'headlines'>
+            <h1 className ='main-headline'>Choice Bot </h1>
+            <h2 className = 'version-text'>v.2.0</h2>
+            <h2 className = 'sub-headline'>Put your fate in my robotic hands puny human. </h2>
+          </div>
+          <img className = 'robot-image' src={require('./choice-bot.jpg')} alt = 'The Choicebot'/>
         </header>
 
         <main className = 'container'>
           {/* Visible on load */}
-          <form className = 'inputNumOptions'>
+          <form className = 'input-num-options'>
             <h3 className = 'question'>How many options do you have?</h3>
-            <input className = 'numOfOptions' type = 'number'/>
-            <button className = 'submitNumInput' type = 'submit'>Submit Options</button>
+            <input className = 'num-of-options' type = 'number'/>
+            <button className = 'submit-num-input' type = 'submit'>Submit Options</button>
           </form>
 
           {/* hidden on load, revealed after number of options */}
 
-          <form className = 'inputOptions'>
+          <form className = 'input-options'>
             <h3 className= 'question'> Enter your options.</h3>
             <div className = 'choices'>
               <input className = 'choice1' type = 'text'/>
               <input className = 'choice1' type = 'text'/>
               <input className = 'choice1' type = 'text'/>
             </div>
-            <button className = 'submitOptions' type = 'submit'>Make a decision.</button>
+            <button className = 'submit-options' type = 'submit'>Make a decision.</button>
           </form>
 
           {/* hidden on load, revealed after number of options */}
@@ -47,7 +50,7 @@ class App extends Component {
 
 
           <div className = 'decision'>
-            <h3 className = 'resultText'>You should choose</h3>
+            <h3 className = 'result-text'>You should choose</h3>
             <h3 className = 'result'>This</h3>
             <button className = 'reset'>Make another choice.</button>
           </div>
