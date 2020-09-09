9549c966f2e014a9234e4da8efafabb6fb49779e
Casey The Great <casey@caseypitman.com>
2020-09-09 18:33:31 -0500

    ship

---
+++ build/precache-manifest.2d5d03875e40250054896b9fbb4a7eb2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edd1bc588e74e2c84c63210366fafb7a",
+    "url": "/choice-bot-2.0/index.html"
+  },
+  {
+    "revision": "84092aea257148a3e402",
+    "url": "/choice-bot-2.0/static/css/main.07a5b863.chunk.css"
+  },
+  {
+    "revision": "3d23ce118c374c8e1071",
+    "url": "/choice-bot-2.0/static/js/2.2f524360.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/choice-bot-2.0/static/js/2.2f524360.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84092aea257148a3e402",
+    "url": "/choice-bot-2.0/static/js/main.a2c60b20.chunk.js"
+  },
+  {
+    "revision": "b03e5477ea4712377183",
+    "url": "/choice-bot-2.0/static/js/runtime-main.c4d58312.js"
+  },
+  {
+    "revision": "b4d2505d0f1dad825508e1b99d54972a",
+    "url": "/choice-bot-2.0/static/media/choice-bot.b4d2505d.jpg"
+  }
+]);
